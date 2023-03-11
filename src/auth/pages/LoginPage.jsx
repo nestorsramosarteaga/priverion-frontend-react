3b3274b814c1091e93cc550f47463af024aeb633
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 11:15:11 -0500

    Update LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,78 @@
+import './LoginPage.css';
 
 export const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-6 login-form-1">
+                    <h3>Login</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input 
+                                type="text"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input 
+                                type="submit"
+                                className="btnSubmit"
+                                value="Login" 
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>Register</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password" 
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Repeat the password" 
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <input 
+                                type="submit" 
+                                className="btnSubmit" 
+                                value="Create Account" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
