31f7aef7a2f76ecbf98bfec3158471975a1386ac
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 10:43:25 -0500

    Basic App

---
+++ src/FrontendApp.jsx
@@ -0,0 +1,6 @@
+
+export const FrontendApp = () => {
+  return (
+    <div>FrontendApp</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import {FrontendApp} from './FrontendApp'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FrontendApp />
   </React.StrictMode>,
 )
