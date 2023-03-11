68d504e6aed02513772293871bb8bf857988cbc0
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 11:49:07 -0500

    Add axios and bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {FrontendApp} from './FrontendApp'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
