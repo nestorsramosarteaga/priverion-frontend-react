d9b1caaeb0c16bddf1c8c53d300cc38e0422e651
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 16:14:01 -0500

    Add employee slice

---
+++ src/store/employee/employeeSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const tempEmployees =  [{
+    "id": 27,
+    "name": "Nata Guerrero",
+    "email": "maria.arteaga@gmail.com",
+    "phone": "+573225001010"
+}, {
+   "id": 28,
+"name": "Néstor Ramos Arteaga",
+"email": "nestor.ramos@gmail.com",
+"phone": "+573126701433"
+}];
+
+export const employeeSlice = createSlice({
+   name: 'employee',
+   initialState: {
+      employees: tempEmployees,
+      activeEmployee: null
+   },
+   reducers: {
+      onSetActiveEmployee: (state, { payload } ) => {
+         state.activeEmployee = payload;
+      },
+   }
+});
+
+
+export const { onSetActiveEmployee } = employeeSlice.actions;
