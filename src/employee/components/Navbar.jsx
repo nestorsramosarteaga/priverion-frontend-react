8442924140d1d5b85dabc2b454bed76f30334924
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 14:38:13 -0500

    Add icon

--- src/employee/components/Navbar.jsx
+++ src/employee/components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
         <span className="navbar-brand">
-            <i class="fa-solid fa-people-group"></i>
+            <i className="fa-solid fa-people-group"></i>
             &nbsp;
             Username
         </span>
