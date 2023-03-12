d0688a59480ec7b6038928a63531f5d2ea0ad14d
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 19:40:00 -0500

    Add Button to Add Employee

--- src/employee/components/FabAddNew.jsx
+++ src/employee/components/FabAddNew.jsx
@@ -1,11 +1,28 @@
+import { useEmployeeStore, useUiStore } from "../../hooks"
 
 
 export const FabAddNew = () => {
-  return (
-    <button
-        className="btn btn-primary fab"
-    >
-        <i className="fa fa-plus"></i>
-    </button>
-  )
+
+    const { openEmployeeModal } = useUiStore();
+    const { setActiveEmployee } = useEmployeeStore();
+
+    const handleClickNew = () => {
+
+        setActiveEmployee({
+          name: '',
+          email: '',
+          phone: ''
+        });
+
+        openEmployeeModal();
+    }
+
+    return (
+        <button
+            className="btn btn-primary fab"
+            onClick={ handleClickNew }
+        >
+            <i className="fa fa-plus"></i>
+        </button>
+    )
 }
